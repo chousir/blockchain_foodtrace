385b0952e6ac526ed61ca1503a0d2594a2b8bf42
Steven <coolboy850319@gmail.com>
2024-03-11 23:39:35 +0800

    add transaction:test and show on flask web

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blockchain_foodtrace
 Use Blockchain to guarantee "Food trace ability" from the farm to the table with complete transparency of the food Supply chain ensuring 100% Food Safety and Trust.
+
+
+export FLASK_APP=blockchain.py
+flask run --port 8000
+
+python run_app.py
--- test.py
+++ test.py
@@ -1,62 +1,72 @@
-from hashlib import sha256
-import json
 import time
+import requests
+import base64
+import ecdsa
+
+def send_transaction(addr_from, addr_to, amount, private_key):
+    if len(private_key) == 64:
+        signature, message = sign_ECDSA_msg(private_key)
+
+        payload = {"from": addr_from,
+                   "to": addr_to,
+                   "amount": amount,
+                   "signature": signature.decode(),
+                   "message": message}
+
+        return transaction(payload)
+    else:
+        return ("Wrong address or key length! Verify and try again.")
+
+def generate_ECDSA_keys():
+
+    sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) #this is your sign (private key)
+    private_key = sk.to_string().hex() #convert your private key to hex
+    vk = sk.get_verifying_key() #this is your verification key (public key)
+    public_key = vk.to_string().hex()
+    #we are going to encode the public key to make it shorter
+    public_key = base64.b64encode(bytes.fromhex(public_key))
+
+    # filename = input("Write the name of your new address: ") + ".txt"
+    # with open(filename, "w") as f:
+    #     f.write("Private key: {0}\nWallet address / Public key: {1}".format(private_key, public_key.decode()))
+    # print("Your new address and private key are now in the file {0}".format(filename))
+    return public_key.decode(),private_key
+
+def sign_ECDSA_msg(private_key):
+    message = str(round(time.time()))
+    bmessage = message.encode()
+    sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.SECP256k1)
+    signature = base64.b64encode(sk.sign(bmessage))
+    return signature, message
+
+def validate_signature(public_key, signature, message):
+    public_key = (base64.b64decode(public_key)).hex()
+    signature = base64.b64decode(signature)
+    vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key), curve=ecdsa.SECP256k1)
+    try:
+        return vk.verify(signature, message.encode())
+    except:
+        return False
+
+def transaction(new_txion):
+        # Then we add the transaction to our list
+        if validate_signature(new_txion['from'], new_txion['signature'], new_txion['message']):
+
+            #print("New transaction")
+            #print("FROM: {0}".format(new_txion['from']))
+            # print("TO: {0}".format(new_txion['to']))
+            # print("AMOUNT: {0}\n".format(new_txion['amount']))
+
+            return "Transaction submission successful\n"
+        else:
+            return "Transaction submission failed. Wrong signature\n"
 
 
-class Block:
-    def __init__(self, index, transactions, timestamp, previous_hash):
-        """
-            Constructor for the `Block` class.
-            :param index:         Unique ID of the block.
-            :param transactions:  List of transactions.
-            :param timestamp:     Time of generation of the block.
-            :param previous_hash: Hash of the previous block in the chain which this block is part of.                                        
-        """
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash # Adding the previous hash field
- 
-    def compute_hash(self):
-        """
-            Returns the hash of the block instance by first converting it
-            into JSON string.
-        """
-        block_string = json.dumps(self.__dict__, sort_keys=True) # The string equivalent also considers the previous_hash field now
-        return sha256(block_string.encode()).hexdigest()
 
-class Blockchain:
- 
-    def __init__(self):
-        """
-            Constructor for the `Blockchain` class.
-        """
-        self.chain = []
-        self.create_genesis_block()
- 
-    def create_genesis_block(self):
-        """
-            A function to generate genesis block and appends it to
-            the chain. The block has index 0, previous_hash as 0, and
-            a valid hash.
-        """
-        genesis_block = Block(0, [], time.time(), "0")
-        genesis_block.hash = genesis_block.compute_hash()
-        self.chain.append(genesis_block)
+public_key,private_key = generate_ECDSA_keys()
 
-genesis_block = Block(0, [], time.time(), "0")
-print(genesis_block)
-genesis_block.hash = genesis_block.compute_hash()
-print(genesis_block.hash)
+print ("public_key:{0}".format(public_key))
+print ("private_key:{0}".format(private_key))
 
-# def compute_hash():
-#     """
-#         Returns the hash of the block instance by first converting it
-#         into JSON string.
-#     """
-#     block_string = json.dumps(self.__dict__, sort_keys=True) # The string equivalent also considers the previous_hash field now
-#     return sha256(block_string.encode()).hexdigest()
-
-# aaa = {'index': 0, 'transactions': [], 'timestamp': 1709814112.8583696, 'previous_hash': '0', 'hash': '9b46a5872a504a9985294ca30c1b638c7b219b0381d5a4fc3312d7679ed2952a'}
-# print((aaa))
-# print(aaa.compute_hash())
+state = send_transaction(public_key, public_key, 1, private_key)
+print (state)
